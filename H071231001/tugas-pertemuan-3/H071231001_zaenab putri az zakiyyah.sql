dd09c9e1c9dec59d6726b0107fbae5573f423c55
Zakiyyah07 <zaenabzakiyyah07@gmail.com>
2024-09-27 16:38:05 +0800

    kumpul tuprak3

---
+++ H071231001/tugas-pertemuan-3/H071231001_zaenab putri az zakiyyah.sql
@@ -0,0 +1,20 @@
+-- nim H071231001
+-- nama zaenab putri az zakiyyah
+
+CREATE DATABASE university;
+
+USE university;
+
+CREATE TABLE mahasiswa(
+	id INT PRIMARY KEY,
+	nama VARCHAR(50),
+	nim CHAR(13) NOT NULL ,
+	prodi VARCHAR(50),
+	ipk DECIMAL(4.00),
+	PRIMARY KEY (id),
+	);
+	
+
+ALTER TABLE namaUniversitas;
+
+
